250deb13222ddbd8dd2a299da496059939d05a21
sishii0193 <Shin@Shinichiros-MacBook-Pro.local>
2014-03-31 10:03:33 -0400

    changes

---
+++ velvetlounge/application.js
@@ -0,0 +1,32 @@
+function showProduct(jewel){
+	$(".overlay").show();
+	$(".details").show();
+var bot = Jewels[jewel];
+$("#detail-title").text(bot.title);
+	$("#detail-price").text("$" + bot.price);
+	$("#detail-main").attr("src", bot.image);
+	$("#detail-description").text(bot.description);
+}
+
+
+	function hideProduct(){
+	$(".overlay").hide();
+	$(".details").hide();
+}
+
+
+
+$(function() {
+$(".product").click(function(){
+		var jewel = $(this).data("product-id");
+	showProduct(jewel);
+});
+
+$(".overlay").click(function(){
+	hideProduct();
+});
+
+
+
+
+});
